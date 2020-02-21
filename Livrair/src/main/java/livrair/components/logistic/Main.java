74e015cadf8a57897dcae69eb3b6387ff48826d8
PERES-Richard <richardperes.info@gmail.com>
2020-02-21 15:40:35 +0100

    #1 #5 Logisitic comp setup

---
+++ Livrair/src/main/java/livrair/components/logistic/Main.java
@@ -0,0 +1,7 @@
+package livrair.components.logistic;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("OHAIO MINA SAN, SORE WA LOJISUTIKKU DESU ! YOROSHIKU NE (❁´◡`❁)");
+    }
+}
