6df7ca108ec433d51084ff45f213f10b63dfbd23
Pierre-Antoine Boulat <pierre-antoine.boulat@etu.unice.fr>
2020-03-13 15:32:01 +0100

    #4 Init (creation arbo, router)

---
+++ Livrair/client/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="secondary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Livrair'"
+          class="shrink mr-2"
+          contain
+          src="../src/assets/drone_green.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+        <span class="mr-2">Livrair</span>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/pns-isa-devops/projet-isa-devops-20-team-f-20"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Git Repo</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ Livrair/client/src/components/Client.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <v-row align="center" justify="center">
+          Client Page
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Client',
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
