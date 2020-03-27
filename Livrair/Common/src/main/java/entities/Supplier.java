7c3c45a73318ea490351b1e81d90366987e3ed4d
PERES-Richard <richardperes.info@gmail.com>
2020-03-27 14:59:50 +0100

    #11 Init persistence

--- Livrair/Common/src/main/java/entities/Supplier.java
+++ Livrair/Common/src/main/java/entities/Supplier.java
@@ -2,15 +2,39 @@ package entities;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Supplier implements Serializable {
 
-    @Id
     private String name;
-
     private String address;
+    private Set<Package> packages;
+
+    @Id
+    @NotNull
+    public String getName() {
+        return name;
+    }
+
+
+    @NotNull
+    public String getAddress() {
+        return address;
+    }
+
+
+    @NotNull
+    @OneToMany(mappedBy="supplier")
+    public Set<Package> getPackages() {
+        return packages;
+    }
+
 
     public Supplier(){}
 
@@ -19,8 +43,34 @@ public class Supplier implements Serializable {
         this.address = address;
     }
 
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPackages(Set<Package> packages) {
+        this.packages = packages;
+    }
+
     @Override
     public String toString() {
-        return name+" "+address;
+        return getName()+" "+getAddress();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Supplier supplier = (Supplier) o;
+        return getName().equals(supplier.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
     }
 }
