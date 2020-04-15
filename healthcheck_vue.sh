f90cd654c34d79086346a2fa757d71a008d4dba2
PERES-Richard <richard.peres@etu.unice.fr>
2020-04-15 21:19:16 +0200

    Update healthcheck_vue.sh

--- healthcheck_vue.sh
+++ healthcheck_vue.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 echo "Vue server healthcheck..."
-sleep 30
+sleep 45
 tries=0
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8844)" != "200" ]]; do
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' dev.richard-peres.com:8844)" != "200" ]]; do
         ((tries=tries+1))
         echo "$tries try..."
-        sleep 30
+        sleep 45
         if [ $tries -eq 5 ];
         then
                 echo "Timeout"
