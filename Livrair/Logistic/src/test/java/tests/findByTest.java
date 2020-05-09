006007fb9aec5c3d65e3acba13df34b6b233c7ad
AnthonyChoquard <anthony.choquard@etu.univ-cotedazur.fr>
2020-05-10 00:04:53 +0200

    fix test logistic #7
    fix du test findbyTest

--- Livrair/Logistic/src/test/java/tests/findByTest.java
+++ Livrair/Logistic/src/test/java/tests/findByTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import arquillian.AbstractLogisticTest;
+import core.DeliveryManager;
 import core.PackageFinder;
 import entities.Package;
 import entities.PackageStatus;
@@ -28,6 +29,8 @@ public class findByTest extends AbstractLogisticTest {
     @EJB
     private PackageFinder packageFinder;
 
+    @EJB
+    private DeliveryManager deliveryManager;
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -35,30 +38,31 @@ public class findByTest extends AbstractLogisticTest {
     @Before
     public void setUp() {
         Supplier ups = new Supplier("UPS", "Cannes");
-        Package pack1 = new Package("2", "testuser1",
+        Package pack1 = new Package("1", "testuser1",
                 "210 avenue roumanille", ups);
         entityManager.persist(pack1);
-        Package pack2 = new Package("3", "testuser1",
+        Package pack2 = new Package("2", "testuser1",
                 "210 avenue das anglais", ups);
         entityManager.persist(pack2);
-        Package pack3 = new Package("4", "testuser2",
+        Package pack3 = new Package("3", "testuser2",
                 "9 rue de la touche", ups);
         entityManager.persist(pack3);
-        Package pack4 = new Package("5", "testuser2",
+        Package pack4 = new Package("4", "testuser2",
                 "9 rue de la touche", ups);
         entityManager.persist(pack4);
-        Package pack5 = new Package("6", "testuser3",
+        Package pack5 = new Package("5", "testuser3",
                 "10 promenade des anglais", ups);
         entityManager.persist(pack5);
+        deliveryManager.changePackageStatut("5",PackageStatus.ASSIGNED);
     }
 
     @Test
     public void findByIdTest(){
         Supplier ups = new Supplier("UPS", "Cannes");
-        Package packTest1 = new Package("3", "testuser1",
+        Package packTest1 = new Package("2", "testuser1",
                 "210 avenue roumanille", ups);
-        assertTrue(packageFinder.findById("2").isPresent());
-        assertEquals(packageFinder.findById("3").get(), packTest1);
+        assertTrue(packageFinder.findById("1").isPresent());
+        assertEquals(packageFinder.findById("2").get(), packTest1);
         assertFalse(packageFinder.findById("999").isPresent());
 
     }
@@ -66,7 +70,7 @@ public class findByTest extends AbstractLogisticTest {
     @Test
     public void findByCustomerTest(){
         Supplier ups = new Supplier("UPS", "Cannes");
-        Package packTest = new Package("4", "testuser2",
+        Package packTest = new Package("3", "testuser2",
                 "9 rue de la touche", ups);
         assertEquals(2, packageFinder.findByCustomer("testuser1").get().size());
         assertEquals(packageFinder.findByCustomer("testuser2").get().get(0), packTest);
@@ -75,16 +79,15 @@ public class findByTest extends AbstractLogisticTest {
     @Test
     public void findByStatusTest(){
         Supplier ups = new Supplier("UPS", "Cannes");
-        Package packTest  = new Package("2", "testuser1",
+        Package packTest  = new Package("1", "testuser1",
                 "210 avenue roumanille", ups);
-        Package packTestbis = new Package("6", "testuser3",
+        Package packTestbis = new Package("5", "testuser3",
                 "10 promenade des anglais", ups);
-
-        // TODO Fix
         packTestbis.setPackageStatus(PackageStatus.ASSIGNED);
-//        assertEquals(4,packageFinder.findByStatus(PackageStatus.REGISTERED).get().size());
-//        assertEquals(packTest, packageFinder.findByStatus(PackageStatus.REGISTERED).get().get(0));
-//        assertEquals(1,packageFinder.findByStatus(PackageStatus.ASSIGNED).get().size());
-//        assertEquals(packTestbis, packageFinder.findByStatus(PackageStatus.ASSIGNED).get().get(0));
+         // TODO Fix
+        assertEquals(4,packageFinder.findByStatus(PackageStatus.REGISTERED).get().size());
+        assertEquals(packTest, packageFinder.findByStatus(PackageStatus.REGISTERED).get().get(0));
+        assertEquals(1,packageFinder.findByStatus(PackageStatus.ASSIGNED).get().size());
+        assertEquals(packTestbis, packageFinder.findByStatus(PackageStatus.ASSIGNED).get().get(0));
     }
 }
