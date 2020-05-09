6b3b47f2356bc5b857b1b2c2eed4e0baad0aa45a
Richard Peres <richardperes.info@gmail.com>
2020-05-09 22:16:06 +0200

    Fix Wipe

--- wipe_hsqldb.sh
+++ wipe_hsqldb.sh
@@ -6,7 +6,7 @@ if [[ "$1" != "" ]]; then
 fi
 
 echo "Remove database inside backend container"
-docker exec backend "rm" "-r" "/usr/local/tomee/data/hsqldb/"
+docker exec backend rm -r /usr/local/tomee/data/hsqldb/
 echo "Restart backend container"
 docker restart backend
 
