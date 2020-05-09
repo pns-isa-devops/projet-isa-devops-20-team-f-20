6ce56befeb89db97207f471e30afd45143162c65
Maxime-LERAS <maxime.leras@etu.unice.fr>
2020-05-09 03:05:43 +0200

    jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "------------------------------------------"
+echo "URL : http://dev.richard-peres.com:8181/"
+echo "Username : admin"
+echo "Password : Drone_Delivery69"
+echo "------------------------------------------"
