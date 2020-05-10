cb8cf940342186f270f8f65ba1ec2a69e03fc3c0
PERES-Richard <richard.peres@etu.unice.fr>
2020-05-10 17:39:31 +0200

    Add page description

--- client.sh
+++ client.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Client App is available on http://localhost:8844/home"
+echo "Service Client page (/client), Mécanicien/Chargeur (/garage) et Manutentionnaire (/manu) are available and functionnal. Gestionnaire (/gestio) is mocked only"
